252a6fbda9b0a97e9480e3fab4feb5e7d39eea69
Jonas Haude <jonashaude@gmx.de>
2019-03-16 12:07:37 +0100

    Initial Commit

---
+++ Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Jonas Haude on 16.03.19.
+//  Copyright © 2019 Jonas Haude. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
